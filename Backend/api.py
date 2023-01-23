243caae3f5ae3031ff274c5023ab87f7fa7dd818
Yang Tan <113662934+Yang-Tan-id@users.noreply.github.com>
2023-01-23 11:34:53 -0600

    Add comments to the function 'getEnergyByMonth'
    Also, add the flask structure.

--- Backend/api.py
+++ Backend/api.py
@@ -1,8 +1,11 @@
+from flask import Flask
 '''
 
 API Deliverable
 
 '''
+
+app = Flask(__name__)
 
 def getTotalEnergyByCategoryOfProduction(categoryOfProduction):
     """Sums energy use across all states and months with a specified category of production
@@ -37,9 +40,24 @@ def getEnergyByState(state):
 
     return 0
 
-def getEnergyByMonth():
-    """
+def getEnergyByMonth(month):
+    """Sums all electricity generation by all catagories in a given month
+    
+    Retrieves columns pertaining to each category of electricity generation for each state in a specific month,
+    sums the values in these columns and returns that number
+    
+    Args:
+        month: an <int>number indicating the specified month（It must be written in the format of month + year, 
+        with no non-numeric symbols in between. For example: 2021/1: 012021; 2020/12: 122020）
+        
+    Returns:
+        returns an integer indicating the sum of the total electricity generation for the
+        provided month
     
     """
     
     return 0
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
