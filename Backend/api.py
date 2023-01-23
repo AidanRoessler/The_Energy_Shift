403d92177918ff70b79155acf46caf8b3467c573
Liam Keane <122481098+liamkeane@users.noreply.github.com>
2023-01-23 17:38:11 -0600

    Meeting edits 1/23
    Co-authored-by: Aidan Roessler <AidanRoessler@users.noreply.github.com>
    Co-authored-by: Yang Tan <Yang-Tan-id@users.noreply.github.com>

--- Backend/api.py
+++ Backend/api.py
@@ -1,16 +1,65 @@
+
 from flask import Flask
-'''
-
-API Deliverable
-
-'''
 
 app = Flask(__name__)
 
-def getTotalEnergyByCategoryOfProduction(categoryOfProduction):
+def isolateDatasetByState(state):
+    """
+        Prunes the working dataset down by the given State (ie. only Wisconsin's data)
+        
+        Retrieves all columns in the current working data set containing the given State, 
+        regardless of category of production.
+        
+        Args:
+            state: a string containing the specified state in written format
+
+        Returns:
+            A paired down version of the data base containing only the columns labeled the specific state(s).
+    """
+    
+    return 0
+
+def isolateDatasetByMonth(month):
+    """
+        Pairs current dataset down by the given month(s) (ie. the data only for March, May, or/and October)
+        
+        Retrieves all columns in the current working datset 
+        for each state that containing the given month.
+
+        Args:
+            month: an integer indicating the specified month(It must be written in the format of month + year, 
+            with no non-numeric symbols in between. For example: 2021/1: 012021; 2020/12: 122020)
+        
+        Returns:
+            A paired down version of the data base containing only the columns labeled the specific month(s).
+
+        
+    """
+    
+    return 0
+
+def isolateDatasetByCategoryOfProduction(categoryOfProduction):
+    '''
+        Pairs data down to just a single catagory of production 
+
+        Retrives all columns in the current working dataset that contain the category of 
+        production specified in the parameter
+
+        Args:
+            categoryOfProduction: a string indicating a specified category of energy production
+        
+        Returns:
+            A paired down version of the data base containing only the columns of the category
+            specified 
+
+    '''
+    return 0
+
+def getTotalEnergyForCategoryOfProduction(categoryOfProduction):
     """Sums energy use across all states and months with a specified category of production
     
-    Retrieves columns pertaining to the given category of production for all states and months, and 
+    Retrieves columns in the current working dataset pertaining to the given category of
+    production for all states and months
 
     Args:
         categoryOfProduction: a string indicating a specified category of energy production
@@ -22,11 +71,11 @@ def getTotalEnergyByCategoryOfProduction(categoryOfProduction):
 
     return 0
 
-def getEnergyByState(state):
+def getEnergyForState(state):
     """Sums all electricity generation by all catagories in a given state
 
-    Retrieves columns pertaining to each category of electricity generation for a single state,
-    sums the values in these columns and returns that number
+    Retrieves columns in the current working dataset pertaining to each category of electricity 
+    generation for a single state, sums the values in these columns and returns that number
 
     Args:
         state: a string indicating the specified state (must spell the states name out,
@@ -40,15 +89,16 @@ def getEnergyByState(state):
 
     return 0
 
-def getEnergyByMonth(month):
+def getEnergyForMonth(month):
     """Sums all electricity generation by all catagories in a given month
     
-    Retrieves columns pertaining to each category of electricity generation for each state in a specific month,
-    sums the values in these columns and returns that number
+    Retrieves columns in the current working dataset pertaining to each category of electricity
+    generation for each state in a specific month, sums the values in these columns and returns 
+    that number
     
     Args:
-        month: an <int>number indicating the specified month（It must be written in the format of month + year, 
-        with no non-numeric symbols in between. For example: 2021/1: 012021; 2020/12: 122020）
+        month: an integer indicating the specified month(It must be written in the format of month + year, 
+        with no non-numeric symbols in between. For example: 2021/1: 012021; 2020/12: 122020)
         
     Returns:
         returns an integer indicating the sum of the total electricity generation for the
@@ -57,6 +107,27 @@ def getEnergyByMonth(month):
     """
     
     return 0
+
+def getRenewableEnergy():
+    '''
+        Sums and returns the total amount of renewable energy for the current state that the dataset has 
+        been "paired down to" 
+
+        Retrieves columns in the current working dataset pertaining to each category of renewable 
+        electricity generation for each state in a specific month, sums the values in these columns 
+        and returns that number
+
+
+        Returns:
+            returns an integer indicating the sum of the total electricity generation from renewable sources 
+            for the current state in the current working dataset
+
+    '''
+
+@app.route("/")
+def home():
+    return "<h1> Home Page!! <h1>"
+
 
 
 if __name__ == '__main__':
--- Backend/cli.py
+++ Backend/cli.py
@@ -4,3 +4,15 @@ Imports functions from api.py,
 Written by: Liam Keane, Aidan Roesller, Rachel Tan
 
 '''
+from api import *
+# The command line program should allow the user to keep selecting functions until they decide to quit.
+# what data will be expected by each function, in what format, etc. It should also demonstrate what data the function calls will return (and in what format).
+
+# display a list of possible functions and how to properly call them
+print("Please enter one of the following function names: \n isolateDatasetByState \n isolateDatasetByMonth \n isolateDatasetByCategoryOfProduction \n getTotalEnergyForCategoryOfProduction \n getEnergyForState \n getEnergyForMonth \n \n ")
+
+# scan user's input
+userInput = input()
+# if input is valid, call the specified function
+
+# print the function's output to the terminal
