2a96c7d7ad6d9c3abd6171105b2c8a11e4d634e5
AidanRoessler <87043349+AidanRoessler@users.noreply.github.com>
2023-01-22 19:41:19 -0600

    Added doc-string for getEnergyByState

--- Backend/api.py
+++ Backend/api.py
@@ -1,10 +1,8 @@
 '''
 
-API Devliverable
+API Deliverable
 
 '''
-
-# First line is summary
 
 def getTotalEnergyByCategoryOfProduction(categoryOfProduction):
     """Sums energy use across all states and months with a specified category of production
@@ -21,9 +19,20 @@ def getTotalEnergyByCategoryOfProduction(categoryOfProduction):
 
     return 0
 
-def getEnergyByState():
-    """
+def getEnergyByState(state):
+    """Sums all electricity generation by all catagories in a given state
+
+    Retrieves columns pertaining to each category of electricity generation for a single state,
+    sums the values in these columns and returns that number
+
+    Args:
+        state: a string indicating the specified state (must spell the states name out,
+        no abbreviations like MN)
     
+    Returns:
+        returns an integer indicating the sum of the total electricity generation for the
+        provided state
+
     """
 
     return 0
