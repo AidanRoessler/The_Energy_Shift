ec59202bc63a2b0511537097e8056c21133a1430
Liam Keane <keanel@carleton.edi>
2023-01-23 20:00:31 -0600

    Finished cli.py and api.py

--- Backend/cli.py
+++ Backend/cli.py
@@ -9,20 +9,50 @@ from api import *
 # what data will be expected by each function, in what format, etc. It should also demonstrate what data the function calls will return (and in what format).
 functionNames = ["isolateDatasetByState", "isolateDatasetByMonth", "isolateDatasetByCategoryOfProduction", "getTotalEnergyForCategoryOfProduction", "getEnergyForState", "getEnergyForMonth", "getRenewableEnergy"]
 # display a list of possible functions and how to properly call them
-print("Please enter one of the following function names: \n ---")
-for function in functionNames:
-    print(function)
+while(1):
+    print("Please enter one of the following function names: \n ---")
+    for function in functionNames:
+        print(function)
 
-print("---")
-# scan user's input
-userInput = input()
-# if input is valid, call the specified function
-if userInput in functionNames:
-    match userInput:
-        case "isolateDatasetByState":
-            userInputedState = input("Please enter a state")
-            isolateDatasetByState(userInputedState)
+    print("---")
+    print("Note: type 'exit' to stop the program")
+    # scan user's input
+    userInput = input()
+    # if input is valid, call the specified function
+    if userInput in functionNames:
+        match userInput:
+            case "isolateDatasetByState":
+                userInputedState = input("This function takes a string as a parameter, modifies the dataset, and then returns void \nPlease enter a state: ")
+                isolateDatasetByState(userInputedState)
+            case "isolateDatasetByMonth":
+                userInputedMonth = input("This function takes an integer representing a month as a paramenter, modifies the dataset, and then returns void. \nPlease enter a month: ")
+                userInputedMonth = int(userInputedMonth)
+                isolateDatasetByMonth(userInputedMonth)
+            case "isolateDatasetByCategoryOfProduction":
+                userInputedSource = input("This function takes a string as a paramenter, modifies the dataset, and then returns void. \nPlease enter a specific source of energy production: ")
+                isolateDatasetByCategoryOfProduction(userInputedSource)
+            case "getTotalEnergyForCategoryOfProduction":
+                userInputedSource = input("This function takes a string as a paramenter and returns the total energy generated via that source as an int in KWH. \nPlease enter a specific source of energy production: ")
+                getTotalEnergyForCategoryOfProduction(userInputedSource)
+            case "getEnergyForState":
+                userInputedState = input("This function takes a string as a paramenter and returns the total energy generated in that State as an int in KWH. \nPlease enter a state: ")
+                getEnergyForState(userInputedState)
+            case "getEnergyForMonth":
+                userInputedMonth = input("This function takes an integer representing a month as a paramenter and returns the total energy generated during that month as an int in KWH. \nPlease enter a month: ")
+                userInputedMonth = int(userInputedMonth)
+                getEnergyForMonth(userInputedMonth)
+            case "getRenewableEnergy":
+                userInputedRenewableSource = input("This function returns the total energy generated via renewable sources as an int in KWH.")
+                getRenewableEnergy()
 
+    elif userInput == "exit":
+        print("Exiting the program")
+        break
+    else:
+        print("That is not a valid function name. ")
+    
+    # prompt the user if they would like to continue
+    userChoice = input("Would you like to continue? Y/N: ")
 
-
-# print the function's output to the terminal
+    if userChoice == "N":
+        break
