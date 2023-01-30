a55de80bbaddcd8c04f88540445bd5f95e047e07
AidanRoessler <87043349+AidanRoessler@users.noreply.github.com>
2023-01-30 13:20:10 -0600

    Modified createtable to create a custom database

--- sqlLab/createtable.sql
+++ sqlLab/createtable.sql
@@ -1,12 +1,8 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
-  quakedate date,
   quaketime time with time zone,
   latitude real,
   longitude real,
   quakedepth real,
-  mag real,
-  id text,
-  place text,
-  quaketype text
+  mag real
 );
