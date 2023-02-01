97092ce8cef4354d0efe7d9c521c870e10cb59de
Liam Keane <122481098+liamkeane@users.noreply.github.com>
2023-02-01 13:38:00 -0600

    created test file

---
+++ Backend/testAPI.py
@@ -0,0 +1,15 @@
+import unittest
+import api
+
+class APITester(unittest.TestCase):
+
+    def test_getEnergyByMonthForState(self):
+
+    def test_ getEnergyByCategoryForState(self):
+        
+    def test_getTotalEnergyForState(self):
+
+    def test_getTotalRenewableEnergyByState(self):
+
+    if __name__ == '__main__':
+        unittest.main()
