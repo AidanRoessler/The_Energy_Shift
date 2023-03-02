68df187ee4e7294b6753f7ee39ce793272ceac6c
Liam Keane <122481098+liamkeane@users.noreply.github.com>
2023-03-01 18:05:39 -0600

    reverting

--- Frontend/static/the_data.js
+++ Frontend/static/the_data.js
@@ -2,7 +2,6 @@ const statesSelect = document.getElementById("statesSelect");
 const statesForm = document.getElementById("statesForm")
 
 statesSelect.addEventListener("change", () => {
-
   statesForm.submit() 
   console.log("event listener entered")
 })
