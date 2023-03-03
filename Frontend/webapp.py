9b9afd6c2c98af8dba061c5a33fa0dc8b6b4f9c2
Liam Keane <122481098+liamkeane@users.noreply.github.com>
2023-03-03 12:04:03 -0600

    added comment

--- Frontend/webapp.py
+++ Frontend/webapp.py
@@ -50,53 +50,7 @@ def aboutTheData():
     
     return render_template('about_the_data.html')
 
-# @app.route('/helloAgain')
-# def templateHome():
-#     '''
-#     Demonstration of rendering an HTML template on the fly, with no variables.
-#     '''
-#     return render_template('hello.html')
-
-# @app.route('/fancyHello')
-# def fancyHome():
-#     '''
-#     Demonstration of rendering an HTML template on the fly, where the HTML template 
-#     contains a stylesheet and an image
-#     '''
-#     return render_template('fancyHello.html')
-
-# @app.route('/greet/<person>/')
-# def greet(person):
-#     '''
-#     Demonstration of passing a parameter into a template.
-#     '''
-#     return render_template('greet.html',
-#                            person=person)
-
-# @app.route('/bigGreet/', methods=['GET'])
-# def greetAgain():
-#     '''
-#     Demonstration of passing multiple parameters into a template.
-#     '''
-#     person = request.args.get('person')
-#     year = request.args.get('classYear')
-#     major = request.args.get('major')
-#     return render_template('biggerGreet.html',
-#                            person=person, year=year, major = major)
-
-# @app.route('/form')
-# def homeWithForm():
-#     '''
-#     Demonstration of rendering a simple form.
-#     '''
-#     return render_template('index.html')    
-
-
-
-'''
-Run the program by typing 'python3 localhost [port]', where [port] is one of 
-the port numbers you were sent by my earlier this term.
-'''
+"""Code can be run with 'python3 webapp.py perlman.mathcs.carleton.edu [ port# ]'"""
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print('Usage: {0} host port'.format(sys.argv[0]), file=sys.stderr)
